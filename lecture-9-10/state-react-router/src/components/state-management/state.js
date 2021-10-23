9602ca20dd970596c489c29113b2ec736db8ddb1
Asad Shah Jahan <asad.shahjahan94@gmail.com>
2021-10-23 13:00:46 +0500

    added if-else statements fo react state management

--- lecture-9-10/state-react-router/src/components/state-management/state.js
+++ lecture-9-10/state-react-router/src/components/state-management/state.js
@@ -6,10 +6,11 @@ export const StateMethod = () => {
       <div>
         <h2>Using the State Hook</h2>
         <p>You reported {count} times</p>
-        <button onClick={() => setCount(count + 1)}>
-          Report post
-        </button>
-        {count > 10 && <div className="alert alert-danger">You cannot report more than 10 times</div>}
+        {count >= 10 ? <div className="alert alert-danger">You cannot report more than 10 times</div> :
+          <button onClick={() => setCount(count + 1)}>
+            Report post
+          </button>
+        }
       </div>
     )
 }
@@ -17,7 +18,26 @@ export const StateMethod = () => {
 export class StateClass extends Component {
     constructor(props) {
         super(props);
-        this.state = { count: 0 }
+        this.state = {
+          count: 0,
+          like: false,
+        }
+    }
+
+    report = () => {
+      // if (this.state.count < 10) {
+        this.setState({ count: this.state.count + 1 })
+      // }
+    }
+
+    unreport = () => {
+      if (this.state.count > 0) {
+        this.setState({ count: this.state.count - 1 })
+      }
+    }
+
+    doLike = () => {
+      this.setState({like: !this.state.like})
     }
 
     render() {
@@ -25,10 +45,22 @@ export class StateClass extends Component {
           <div>
             <h2>Using the Class State</h2>
             <p>You reported {this.state.count} times</p>
-            <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+            <button onClick={this.report}>
               Report post
             </button>
-            {this.state.count > 10 && <div className="alert alert-danger">You cannot report more than 10 times</div>}
+            <button onClick={this.unreport}>
+              Unreport post
+            </button>
+            <br/>
+            <hr/>
+            <h3 onClick={this.doLike}>{this.state.like ? "Unlike" : "Like"}</h3>
+            {this.state.count > 0 && (
+              this.state.count > 0 && this.state.count < 10 ?
+              <div className="alert alert-info">reported</div> : (
+                this.state.count >= 10 && this.state.count <=20 ? <div className="alert alert-warning">warning regarding report</div> :
+                  <div className="alert alert-danger">error regarding report</div>
+              )
+            )}
           </div>
         );
       }
